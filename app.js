53171c284c27f3d42daf32d324491cd6f830f8e9
Hari Sehgal <harisehgal34@gmail.com>
2025-07-03 14:15:09 +0530

    JavaScript

---
+++ app.js
@@ -0,0 +1,53 @@
+console.log("Hello World");
+console.log("Apna College");
+let a = 7;
+let b = 5;
+console.log("Sum is :",a+b); //Statement to print sum of a and b
+console.log(`Total sum is : ${a+b}. This is print using template.`);
+
+//Arithmetic Operations
+console.log("Addition :",a+b);
+console.log("Subtraction :",a-b);
+console.log("Multiplication :",a*b); 
+console.log("Division :",a/b); 
+console.log("Modulo :",a%b);
+console.log("Exponential :",a**b);
+
+//Unary Operations
+console.log("Post Increment :",a++);
+console.log("Pre Increment :",++a);
+console.log("Post Decrement :",a--);
+console.log("Pre Decrement :",--a);
+
+//Assignment Operations
+a = 10;
+a += 1;
+console.log("Value of a :",a);
+a -= 1;
+console.log("Value of a :",a);
+a *=2;
+console.log("Value of a :",a);
+a /=2;
+console.log("Value of a :",a);
+
+//Comparison Operations
+let age = 18;
+console.log("Greater than :",age>18);
+console.log("Greater than or equal to :",age>=18);
+console.log("Lesser than :",age<18);
+console.log("Lesser than or equal to :",age<=18);
+let str = '18';
+console.log("Equal to :",age == str);
+console.log("Not equal to:",age != str);
+console.log("Triple equal to :",age === str);
+
+//Logical Operations
+console.log("true && true : ",true&&true);
+console.log("true && false : ",true&&false);
+console.log("false && false : ",false&&false);
+console.log("true || true : ",true||true);
+console.log("true || false : ",true||false);
+console.log("false || false : ",false||false);
+console.log('! true :', ! true);
+console.log('! false :', ! false);
+
