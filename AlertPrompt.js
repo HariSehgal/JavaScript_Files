b03702c000c803af89020acb4fa6114cb2b75374
Hari Sehgal <harisehgal34@gmail.com>
2025-07-03 14:12:59 +0530

    JavaScript

---
+++ AlertPrompt.js
@@ -0,0 +1,5 @@
+alert("This is an alert");
+let firstName = prompt("Enter first name");
+let lastName = prompt("Enter last name");
+let Name = firstName + " " + lastName;
+console.log("Name is : ", Name);
