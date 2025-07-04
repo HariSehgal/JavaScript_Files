eec3666d83c8fdb82c0a6c4d911f46fe6e42f870
Hari Sehgal <harisehgal34@gmail.com>
2025-07-04 15:00:22 +0530

    JavaScript

---
+++ ArrayMethod.js
@@ -0,0 +1,9 @@
+let cars = ["Audi","Bmw","Maruti","Xuv"];
+cars.push("Ferrari");
+console.log(cars);
+let car_name = cars.pop();
+console.log(car_name);
+cars.unshift("Ferrari");
+console.log(cars);
+car_name = cars.shift();
+console.log(car_name);
