afe147bf8c4cc9c9696159eefd23141e91fa2164
Hari Sehgal <harisehgal34@gmail.com>
2025-07-05 16:16:55 +0530

    JavaScript

---
+++ AS14q1_to_3.js
@@ -0,0 +1,23 @@
+//Question 1
+let dice = Math.floor(Math.random()*6) + 1;
+console.log(dice);
+
+//Question 2
+let car = {
+    name : "Rolls Royce",
+    model : "Phantom",
+    color : "Black"
+};
+console.log(`Car name : ${car.name}`);
+
+//Question 3
+let person = {
+    name : "Hari Sehgal",
+    age : 19,
+    city : "Delhi"
+};
+
+person.city = "New York";
+person.country = "United States"
+
+console.log(person);
