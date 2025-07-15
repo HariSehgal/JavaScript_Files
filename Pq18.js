fe2582b75c9a0b783283061465355b721d7d7be9
Hari Sehgal <harisehgal34@gmail.com>
2025-07-15 12:55:57 +0530

    JavaScript

---
+++ Pq18.js
@@ -0,0 +1,11 @@
+let color = 'yellow';
+
+if(color == 'red'){
+    console.log("Stop. Light color is red");
+}else if(color == 'yellow'){
+    console.log("Slow Down. Light color is yellow");
+}else if(color == 'green'){
+    console.log("Go. Light color is green");
+}else{
+    console.log("Invalid color")
+}
