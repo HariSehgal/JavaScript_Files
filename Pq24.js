fdd7eede9e377f0985201dc56c512eae64a1d8d9
Hari Sehgal <harisehgal34@gmail.com>
2025-07-21 12:28:19 +0530

    JavaScript

---
+++ Pq24.js
@@ -0,0 +1,9 @@
+let months = ['january' , 'july' , 'march' , 'august'];
+months.splice(0,2,"july","june");
+console.log(months);
+
+//Question 2
+let lang = ['c','c++','html','javascript','python','java','c#','sql'];
+lang.reverse();
+console.log(lang);
+console.log(lang.indexOf("javascript"));
